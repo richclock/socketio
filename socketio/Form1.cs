31c9340ec1ae53980e574195d9896df6e3c44e55
richclock <richclock@yahoo.com.tw>
2024-03-06 14:37:54 +0800

    first commit

---
+++ socketio/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Quobject.SocketIoClientDotNet.Client;
+
+namespace socketio
+{
+    public partial class Form1 : Form
+    {
+        Socket socket;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            socket= IO.Socket("http://localhost:3000");
+            socket.On(Socket.EVENT_CONNECT, (data) => {
+                if (data != null) {
+                    MessageBox.Show(data.ToString());
+                    Console.WriteLine(data);
+                    //socket.Disconnect();
+                }
+            });
+
+            //socket.On("deal", (data) =>
+            //{
+            //    MessageBox.Show(data.ToString());
+            //    Console.WriteLine(data);
+            //    //socket.Disconnect();
+            //});
+            socket.Connect();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            socket.Disconnect();
+        }
+    }
+}
